c13fa742e5fc56ef77dca9ef17bbb85c243d8bfc
Brent Watson <brentmwatson@gmail.com>
2016-11-11 13:47:18 -0500

    Handle bars working

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
+// variabbles to use Handlebars
+var source   = $("#notes-template").html();
+var note_template = Handlebars.compile(source);
+
+// handlebars used in function
 $.getJSON('https://shielded-beach-33937.herokuapp.com/api/notes').then(function(recall_notes) {
-      recall_notes.notes.forEach(function(notes){
-        $('#displaynotes').prepend('<div>' + notes.title + '</div><div>' + notes.body+ '</div>')
-        console.log(recall_notes)
+      recall_notes.notes.forEach(function(note){
+      var note_display = note_template (note)
+      $('#displaynotes').prepend(note_display)
       })
 })
-
-// then(function(response){
-//      response.photos.forEach(function(photo){
-//        $('#photos').prepend('<img src="https://desolate-sands-90495.herokuapp.com/' + photo.photo + '" width=200 /><p>' + photo.caption + '</p>')
